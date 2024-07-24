5f1117cd996f0e0f3548b350b71083581d5ca24a
Piyushraj Bista <piyushrajbista@outlook.com>
2024-07-24 09:04:53 +0545

    feat: create ICourseRepository interface for managing courses

---
+++ DigiShikshya.Application/Contracts/Course/ICourseRepository.cs
@@ -0,0 +1,8 @@
+public interface ICourseRepository
+{
+    Task<PaginatedResult<Course>> GetAllCourses(CourseListQuery request);
+    Task<Course> GetCourseById(Guid id);
+    Task<bool> AddCourse(Course course);
+    Task<bool> UpdateCourse(Course course);
+    Task<bool> DeleteCourse(Guid id);
+}
