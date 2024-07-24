2ac45661de84adfb7a420f09338689044e0023bb
Piyushraj Bista <piyushrajbista@outlook.com>
2024-07-24 08:18:44 +0545

    refactor: update Base class to use DateTime.Now instead of DateTime.UtcNow

--- DigiShikshya.Domain/Entities/Base/Base.cs
+++ DigiShikshya.Domain/Entities/Base/Base.cs
@@ -1,8 +1,8 @@
 public abstract class Base
 {
     public Guid Id { get; set; } = Guid.NewGuid();
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime UpdatedAt { get; set; } = DateTime.Now;
     public string? CreatedBy { get; set; }
     public string? UpdatedBy { get; set; }
     public bool IsActive { get; set; } = true;
