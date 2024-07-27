366d431980af1a6d497717092a2fe6071f1e97d6
Piyushraj Bista <piyushrajbista@outlook.com>
2024-07-27 23:44:47 +0545

    refactor: improve error handling and validation

--- DigiShikshya.Application/Features/Course/Command/AddNewCourseHandler.cs
+++ DigiShikshya.Application/Features/Course/Command/AddNewCourseHandler.cs
@@ -1,32 +1,36 @@
+using System.ComponentModel.DataAnnotations;
 using MediatR;
 
 public class AddNewCourseHandler(ICourseRepository _courseRepository) : IRequestHandler<AddNewCourse, AddNewCourseResponse>
 {
     public async Task<AddNewCourseResponse> Handle(AddNewCourse request, CancellationToken cancellationToken)
     {
-        var response = new AddNewCourseResponse();
         var validator = new AddNewCourseValidator();
         var validationResult = validator.Validate(request);
 
-        if (validationResult.IsValid)
+        if (!validationResult.IsValid)
         {
-            var course = new Course
+            return new AddNewCourseResponse
             {
-                Name = request.Name,
-                Description = request.Description
+                Status = "Bad Request",
+                Message = "Validation failed",
+                Errors = validationResult.Errors.Select(x => x.ErrorMessage).ToList()
             };
-
-            var success = await _courseRepository.AddCourse(course);
-            response.IsSuccess = success;
-            response.Message = success ? ["Course added successfully"] : ["Failed to add course"];
-            return response;
         }
-        else
+
+        var course = new Course
         {
-            response.Message = validator.Validate(request).Errors.Select(x => x.ErrorMessage).ToList();
-            response.IsSuccess = false;
-            return response;
+            Name = request.Name,
+            Description = request.Description
+        };
 
-        }
+        var success = await _courseRepository.AddCourse(course);
+
+        return new AddNewCourseResponse
+        {
+            Status = success ? "Success" : "Failed",
+            Message = success ? "Course added successfully" : "Failed to add course",
+            Errors = ["Something went wrong, please try again later"]
+        };
     }
 }
