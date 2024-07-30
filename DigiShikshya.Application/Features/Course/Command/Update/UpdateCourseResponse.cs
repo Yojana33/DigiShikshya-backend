5b3e99969a87c770ee3b83cbba076a2f249c8ff8
Yojana <yosulalala@gmail.com>
2024-07-30 12:41:05 +0545

    "feat: added update course response"

---
+++ DigiShikshya.Application/Features/Course/Command/Update/UpdateCourseResponse.cs
@@ -0,0 +1,10 @@
+public class UpdateCourseResponse
+{
+    public List<string>? Message { get; set; }
+
+    public DateTime? RequestedAt { get; set; } = DateTime.Now;
+
+    public string? Status { get; set; }
+    public bool? IsSuccess { get; set; }
+
+}
