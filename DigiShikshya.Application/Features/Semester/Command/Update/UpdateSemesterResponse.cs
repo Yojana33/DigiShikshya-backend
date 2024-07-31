9b9c01a357c1f729912a9ee51a881b59eef77cd8
Yojana <yosulalala@gmail.com>
2024-07-31 16:23:11 +0545

    "feat: created models for update semester response"

---
+++ DigiShikshya.Application/Features/Semester/Command/Update/UpdateSemesterResponse.cs
@@ -0,0 +1,10 @@
+public class UpdateSemesterResponse
+{
+    public List<string>? Message { get; set; }
+
+    public DateTime? RequestedAt { get; set; } = DateTime.Now;
+
+    public string? Status { get; set; }
+    public bool? IsSuccess { get; set; }
+
+}
