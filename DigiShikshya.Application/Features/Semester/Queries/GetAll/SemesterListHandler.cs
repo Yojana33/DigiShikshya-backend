46e6c5c428dcb8668649662c7d0aad59ab74b1fa
Yojana <yosulalala@gmail.com>
2024-07-31 16:25:04 +0545

    "feat: created handler for semester list"

---
+++ DigiShikshya.Application/Features/Semester/Queries/GetAll/SemesterListHandler.cs
@@ -0,0 +1,35 @@
+using MediatR;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+class SemesterListHandler(ISemesterRepository _semesterRepository) : IRequestHandler<SemesterListQuery, PaginatedResult<SemesterListResponse>>
+{
+    public async Task<PaginatedResult<SemesterListResponse>> Handle(SemesterListQuery request, CancellationToken cancellationToken)
+    {
+        var semesters = await _semesterRepository.GetAllSemesters(request);
+
+        var response = new PaginatedResult<SemesterListResponse>
+        {
+            Items = semesters?.Items?.Select(x => new SemesterListResponse
+            {
+                Id = x.Id,
+                SemesterName = x.SemesterName,
+                StartDate = x.StartDate,
+                EndDate = x.EndDate,
+                CreatedAt = x.CreatedAt,
+                UpdatedAt = x.UpdatedAt,
+                CreatedBy = x.CreatedBy,
+                UpdatedBy = x.UpdatedBy,
+                IsActive = x.IsActive
+
+            }).ToList(),
+            Page = semesters.Page,
+            PageSize = semesters.PageSize,
+            TotalCount = semesters.TotalCount,
+            TotalPages = semesters.TotalPages
+        };
+
+        return response;
+    }
+}
