7f34c9faf68b5ca94636f160c812adcbc38197c0
Yojana <yosulalala@gmail.com>
2024-07-31 16:24:05 +0545

    "feat: created models for semester list response"

---
+++ DigiShikshya.Application/Features/Semester/Queries/GetAll/SemesterListResponse.cs
@@ -0,0 +1,12 @@
+public class SemesterListResponse
+{
+    public Guid Id { get; set; }
+    public string? SemesterName { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public string? CreatedBy { get; set; }
+    public string? UpdatedBy { get; set; }
+    public bool IsActive { get; set; }
+}
