efdb97afcd80b6e0a29a1d5fc377866738ff78ca
Yojana <yosulalala@gmail.com>
2024-07-31 16:12:26 +0545

    DigiShikshya.Application/Features/Semester/

---
+++ DigiShikshya.Application/Contracts/Semester/ISemesterRepository.cs
@@ -0,0 +1,8 @@
+public interface ISemesterRepository
+{
+    Task<PaginatedResult<Semester>> GetAllSemesters(SemesterListQuery request);
+    Task<Semester> GetSemesterById(Guid id);
+    Task<bool> AddSemester(Semester semester);
+    Task<bool> UpdateSemester(Semester semester);
+    Task<bool> DeleteSemester(Guid id);
+}
