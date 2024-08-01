6cd477975fe71db7a219e0ddcce7019391f21a0e
Yojana <yosulalala@gmail.com>
2024-08-01 16:05:06 +0545

    "feat: added required validation in description"

--- DigiShikshya.Application/Features/Course/Command/Add/AddNewCourseValidator.cs
+++ DigiShikshya.Application/Features/Course/Command/Add/AddNewCourseValidator.cs
@@ -5,6 +5,7 @@ class AddNewCourseValidator:AbstractValidator<AddNewCourse>
     public AddNewCourseValidator()
     {
         RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required").WithErrorCode("400");
+        RuleFor(x => x.Description).NotEmpty().WithMessage("Description is required").WithErrorCode("400");
     }
     
 }
