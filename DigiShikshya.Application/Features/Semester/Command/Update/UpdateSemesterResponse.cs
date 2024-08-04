9dbb72fabc0e3f29913a2618e306c76ed41eeda3
Yojana <yosulalala@gmail.com>
2024-08-04 13:19:39 +0545

    "feat: added errors in update semester response"

--- DigiShikshya.Application/Features/Semester/Command/Update/UpdateSemesterResponse.cs
+++ DigiShikshya.Application/Features/Semester/Command/Update/UpdateSemesterResponse.cs
@@ -1,10 +1,12 @@
 public class UpdateSemesterResponse
 {
-    public List<string>? Message { get; set; }
+    public string? Message { get; set; }
 
     public DateTime? RequestedAt { get; set; } = DateTime.Now;
 
     public string? Status { get; set; }
     public bool? IsSuccess { get; set; }
+
+    public List<string>? Errors { get; set; }
 
 }
