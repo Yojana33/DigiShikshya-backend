2465e7373af6e14275b148b843a78e765cea93e3
Yojana <yosulalala@gmail.com>
2024-08-05 11:37:10 +0545

    "feat: script for creating course table"

---
+++ DigiShikshya.Persistence/Migration/1-create_course_table_script.sql
@@ -0,0 +1,11 @@
+CREATE TABLE course
+(
+    id UUID PRIMARY KEY,
+    course_name VARCHAR(255) NOT NULL,
+    course_description TEXT NOT NULL,
+    created_by VARCHAR(255),
+    modified_by VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    is_active BOOLEAN DEFAULT TRUE
+);
