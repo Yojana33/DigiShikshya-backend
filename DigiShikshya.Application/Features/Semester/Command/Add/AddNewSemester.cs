e4b641e2591ea9cd0b5cf2a6b013191298037e58
Yojana <yosulalala@gmail.com>
2024-08-05 11:33:40 +0545

    refactor: used date time for dates in add semester

--- DigiShikshya.Application/Features/Semester/Command/Add/AddNewSemester.cs
+++ DigiShikshya.Application/Features/Semester/Command/Add/AddNewSemester.cs
@@ -3,6 +3,6 @@ using MediatR;
 public class AddNewSemester : IRequest<AddNewSemesterResponse>
 {
     public string? SemesterName { get; set; }
-    public DateOnly StartDate { get; set; }
-    public DateOnly EndDate { get; set; }
+    public required DateTime StartDate { get; set; }
+    public required DateTime EndDate { get; set; }
 }
