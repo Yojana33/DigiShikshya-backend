bc03305cbfbea49c46d8be2ab7ad6f4184d39eb5
Yojana <yosulalala@gmail.com>
2024-08-05 12:43:03 +0545

    refactor: added course_id in entity of semester

--- DigiShikshya.Domain/Entities/Semester/Semester.cs
+++ DigiShikshya.Domain/Entities/Semester/Semester.cs
@@ -1,5 +1,6 @@
 public class Semester : Base
 {
+    public Guid CourseId { get; set; }
     public string? SemesterName { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
