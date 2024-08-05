3a04b0fc38bf4befacd0b92e38b59caa500dbf2a
Yojana <yosulalala@gmail.com>
2024-08-05 11:34:18 +0545

    refactor: used datetime for dates in update semester

--- DigiShikshya.Application/Features/Semester/Command/Update/UpdateSemester.cs
+++ DigiShikshya.Application/Features/Semester/Command/Update/UpdateSemester.cs
@@ -5,6 +5,6 @@ public class UpdateSemester : IRequest<UpdateSemesterResponse>
 {
     public required Guid Id { get; set; }
     public string? NewName { get; set; }
-    public DateOnly? NewStartDate { get; set; }
-    public DateOnly? NewEndDate { get; set; }
+    public DateTime? NewStartDate { get; set; }
+    public DateTime? NewEndDate { get; set; }
 }
