0c8d9711d4c035ada70b2c1274184b518966226c
Piyushraj Bista <piyushrajbista@outlook.com>
2024-08-07 04:36:09 +0545

    refactor: use DateTime

--- DigiShikshya.Application/Features/Semester/Queries/GetAll/SemesterListResponse.cs
+++ DigiShikshya.Application/Features/Semester/Queries/GetAll/SemesterListResponse.cs
@@ -2,8 +2,8 @@ public class SemesterListResponse
 {
     public Guid Id { get; set; }
     public string? SemesterName { get; set; }
-    public DateOnly StartDate { get; set; }
-    public DateOnly EndDate { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
     public string? CreatedBy { get; set; }
