87558aed2dfffe4fd4007244cef9dba945ec0f20
Yojana <yosulalala@gmail.com>
2024-08-16 12:19:20 +0545

    feat: created model for adding new batch

---
+++ DigiShikshya.Application/Features/Batch/Command/Add/AddNewBatch.cs
@@ -0,0 +1,9 @@
+using MediatR;
+
+public class AddNewBatch : IRequest<AddNewBatchResponse>
+{
+    public Guid CourseId { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public int Status { get; set; }
+}
