643d542d7549cd426582271f46a12b0248eabd40
Yojana <yosulalala@gmail.com>
2024-08-16 12:41:05 +0545

    feat: created validator for adding new batch

---
+++ DigiShikshya.Application/Features/Batch/Command/Add/AddNewBatchValidator.cs
@@ -0,0 +1,20 @@
+using FluentValidation;
+
+public class AddNewBatchValidator : AbstractValidator<AddNewBatch>
+{
+    public AddNewBatchValidator()
+    {
+        RuleFor(x => x.CourseId)
+            .NotEmpty().WithMessage("Course ID is required").WithErrorCode("400");
+
+        RuleFor(x => x.StartDate)
+            .NotEmpty().WithMessage("Start date is required").WithErrorCode("400")
+            .LessThan(x => x.EndDate).WithMessage("Start date must be before the end date").WithErrorCode("400");
+
+        RuleFor(x => x.EndDate)
+            .NotEmpty().WithMessage("End date is required").WithErrorCode("400");
+
+        RuleFor(x => x.Status)
+            .IsInEnum().WithMessage("Invalid status value").WithErrorCode("400");
+    }
+}
