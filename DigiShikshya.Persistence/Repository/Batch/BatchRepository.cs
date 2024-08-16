a421a9c0351ab61dda7c9c6a5944b60a0029ac01
Yojana <yosulalala@gmail.com>
2024-08-16 13:27:31 +0545

    feat: implemented method to add batch

---
+++ DigiShikshya.Persistence/Repository/Batch/BatchRepository.cs
@@ -0,0 +1,46 @@
+using System.Data;
+using System.Threading.Tasks;
+using Dapper;
+
+public class BatchRepository : IBatchRepository
+{
+    private readonly IDbConnection _dbConnection;
+
+    public BatchRepository(IDbConnection dbConnection)
+    {
+        _dbConnection = dbConnection;
+        Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
+    }
+
+    public async Task<bool> AddBatch(Batch batch)
+    {
+        var query = "INSERT INTO batch (id, course_id, start_date, end_date, status, created_at) VALUES (@BatchId, @CourseId, @StartDate, @EndDate, @Status, @CreatedAt)";
+        await _dbConnection.ExecuteAsync(query, batch);
+        return true;
+    }
+
+    public Task<bool> BatchAlreadyExists(Guid courseId, DateTime startDate, DateTime endDate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteBatch(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<PaginatedResult<BatchListResponse>> GetAllBatches(BatchListQuery request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Batch> GetBatchById(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UpdateBatch(Batch batch)
+    {
+        throw new NotImplementedException();
+    }
+}
