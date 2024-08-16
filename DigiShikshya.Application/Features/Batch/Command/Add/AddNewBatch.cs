2f64303d1b2a3dec83daf4738063a497d53ddeaa
Yojana <yosulalala@gmail.com>
2024-08-16 13:43:26 +0545

    fix: fixed type of status from int to status enum

--- DigiShikshya.Application/Features/Batch/Command/Add/AddNewBatch.cs
+++ DigiShikshya.Application/Features/Batch/Command/Add/AddNewBatch.cs
@@ -5,5 +5,5 @@ public class AddNewBatch : IRequest<AddNewBatchResponse>
     public Guid CourseId { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
-    public int Status { get; set; }
+    public StatusEnum Status { get; set; }
 }
