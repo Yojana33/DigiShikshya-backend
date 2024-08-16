bd1949e0d7a34a98d45d5941d5def399a70e1b60
Yojana <yosulalala@gmail.com>
2024-08-16 13:32:04 +0545

    feat: created endpoint to add batch

---
+++ DigiShikshya.API/Controller/Batch/BatchController.cs
@@ -0,0 +1,28 @@
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/v1/batch")]
+public class BatchController(IMediator mediator) : ControllerBase
+{
+    private readonly IMediator _mediator = mediator;
+
+    [HttpPost("add")]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> AddBatch(AddNewBatch request)
+    {
+        var response = await _mediator.Send(request);
+
+        return response.Status switch
+        {
+            "Success" => CreatedAtAction(nameof(AddBatch), response),
+            "Bad Request" => BadRequest(response),
+            "Internal Server Error" => StatusCode(StatusCodes.Status500InternalServerError, response),
+            _ => StatusCode(StatusCodes.Status500InternalServerError, "An unexpected error occurred.")
+        };
+    }
+}
