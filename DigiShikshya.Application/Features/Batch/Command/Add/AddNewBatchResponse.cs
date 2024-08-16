e11edc20c2efa702315315bd5ab73318c643e20c
Yojana <yosulalala@gmail.com>
2024-08-16 12:21:48 +0545

    feat: created response model for add new batch

---
+++ DigiShikshya.Application/Features/Batch/Command/Add/AddNewBatchResponse.cs
@@ -0,0 +1,7 @@
+public class AddNewBatchResponse
+{
+    public string? Message { get; set; }
+    public DateTime? RequestedAt { get; set; } = DateTime.Now;
+    public string? Status { get; set; }
+    public List<string>? Errors { get; set; }
+}
