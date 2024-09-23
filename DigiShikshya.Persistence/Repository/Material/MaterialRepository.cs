f67e174aad2f5901976b4d0f4d50d29c4790f386
Yojana <yosulalala@gmail.com>
2024-09-23 12:55:48 +0545

    feat: written query for add material

---
+++ DigiShikshya.Persistence/Repository/Material/MaterialRepository.cs
@@ -0,0 +1,45 @@
+using System.Data;
+using Dapper;
+using System.Threading.Tasks;
+
+public class MaterialRepository : IMaterialRepository
+{
+    private readonly IDbConnection _dbConnection;
+
+    public MaterialRepository(IDbConnection dbConnection)
+    {
+        _dbConnection = dbConnection;
+        Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
+    }
+
+    public async Task<bool> AddMaterial(Material material)
+    {
+        const string query = @"
+        INSERT INTO material (id, subject_id, title, description, content_type, content, upload_date, uploaded_by, created_at)
+        VALUES (@Id, @SubjectId, @Title, @Description, @ContentType, @Content, @UploadDate, @UploadedBy, @CreatedAt)";
+
+        await _dbConnection.ExecuteScalarAsync<bool>(query, material);
+
+        return true;
+    }
+
+    public Task<bool> DeleteMaterial(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<PaginatedResult<MaterialListResponse>> GetAllMaterials(MaterialListQuery request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<MaterialListResponse> GetMaterialById(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UpdateMaterial(Material material)
+    {
+        throw new NotImplementedException();
+    }
+}
