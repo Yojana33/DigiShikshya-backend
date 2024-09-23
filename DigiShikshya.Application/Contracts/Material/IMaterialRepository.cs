9d132120d4b7c7df739293c67b8a02cd08c35e97
Yojana <yosulalala@gmail.com>
2024-09-23 11:53:41 +0545

    feat: created interface for material

---
+++ DigiShikshya.Application/Contracts/Material/IMaterialRepository.cs
@@ -0,0 +1,8 @@
+public interface IMaterialRepository
+{
+    Task<PaginatedResult<MaterialListResponse>> GetAllMaterials(MaterialListQuery request);
+    Task<MaterialListResponse> GetMaterialById(Guid id);
+    Task<bool> AddMaterial(Material material);
+    Task<bool> UpdateMaterial(Material material);
+    Task<bool> DeleteMaterial(Guid id);
+}
