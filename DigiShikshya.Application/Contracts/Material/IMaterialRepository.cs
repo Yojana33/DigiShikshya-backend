07e51a95afefed1b597df3ca2ce25411af8c6212
Yojana <yosulalala@gmail.com>
2024-09-24 16:00:45 +0545

    feat:method declaration to get video content.

--- DigiShikshya.Application/Contracts/Material/IMaterialRepository.cs
+++ DigiShikshya.Application/Contracts/Material/IMaterialRepository.cs
@@ -5,4 +5,5 @@ public interface IMaterialRepository
     Task<bool> AddMaterial(Material material);
     Task<bool> UpdateMaterial(Material material);
     Task<bool> DeleteMaterial(Guid id);
+    Task<byte[]> GetVideoContentById(Guid id);
 }
