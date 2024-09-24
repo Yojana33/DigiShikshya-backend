669aba32fded540668a84cf5d08c88f08e673a8c
Yojana <yosulalala@gmail.com>
2024-09-24 16:01:39 +0545

    feat: service registration in infrastructure

--- DigiShikshya.Infrastructure/InfrastructureServiceRegistration.cs
+++ DigiShikshya.Infrastructure/InfrastructureServiceRegistration.cs
@@ -1,6 +1,15 @@
+using DigiShikshya.Infrastructure.Services;
+using Microsoft.Extensions.DependencyInjection;
+
 public static class InfrastructureServiceRegistration
 {
+    public static void AddInfrastructureServices(this IServiceCollection services)
+    {
+        //Register database connection
+        services.AddScoped<VideoService>();
 
+
+    }
 
 
 }
