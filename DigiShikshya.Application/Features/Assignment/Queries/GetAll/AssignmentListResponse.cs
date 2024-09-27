83a53b5f1e8b0ebd37a8060c7c5c7be0b9481d62
Yojana <yosulalala@gmail.com>
2024-09-27 16:48:45 +0545

    feat: response model for assignment list

---
+++ DigiShikshya.Application/Features/Assignment/Queries/GetAll/AssignmentListResponse.cs
@@ -0,0 +1,12 @@
+public class AssignmentListResponse
+{
+    public Guid Id { get; set; }
+    public Guid SubjectId { get; set; }
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public DateTime DueDate { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public string? CreatedBy { get; set; }
+    public string? UpdatedBy { get; set; }
+}
