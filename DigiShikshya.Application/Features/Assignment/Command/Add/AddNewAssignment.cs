05fe4ed09f8e2b0f1ce71ad2fd5009f0662032b4
Yojana <yosulalala@gmail.com>
2024-09-29 11:25:30 +0545

    refactor: added teacher id in model of add new assignment

--- DigiShikshya.Application/Features/Assignment/Command/Add/AddNewAssignment.cs
+++ DigiShikshya.Application/Features/Assignment/Command/Add/AddNewAssignment.cs
@@ -3,6 +3,7 @@ using MediatR;
 public class AddNewAssignment : IRequest<AddNewAssignmentResponse>
 {
     public Guid SubjectId { get; set; }
+    public Guid TeacherId { get; set; }
     public string? Title { get; set; }
     public string? Description { get; set; }
     public DateTime DueDate { get; set; }
