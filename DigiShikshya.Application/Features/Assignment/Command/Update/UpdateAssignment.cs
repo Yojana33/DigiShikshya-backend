1c2d16e117aacf0c47ac1773cccd4178e02bbb8b
Yojana <yosulalala@gmail.com>
2024-09-29 11:32:16 +0545

    refactor: teacher id in model of update assignment

--- DigiShikshya.Application/Features/Assignment/Command/Update/UpdateAssignment.cs
+++ DigiShikshya.Application/Features/Assignment/Command/Update/UpdateAssignment.cs
@@ -5,6 +5,7 @@ public class UpdateAssignment : IRequest<UpdateAssignmentResponse>
 {
     public required Guid Id { get; set; }
     public Guid NewSubjectId { get; set; }
+    public Guid NewTeacherId { get; set; }
     public string? NewTitle { get; set; }
     public string? NewDescription { get; set; }
     public DateTime NewDueDate { get; set; }
