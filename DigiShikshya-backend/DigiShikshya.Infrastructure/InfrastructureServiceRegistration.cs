294ef93f16a1a4268d75e9fcb93c55c7804e40c5
techmologist <piyushrajbista@outlook.com>
2024-09-30 16:47:30 +0545

    chore: Add HttpClient configuration for KeyCloak service

--- DigiShikshya-backend/DigiShikshya.Infrastructure/InfrastructureServiceRegistration.cs
+++ DigiShikshya-backend/DigiShikshya.Infrastructure/InfrastructureServiceRegistration.cs
@@ -1,15 +1,21 @@
 using DigiShikshya.Infrastructure.Hubs;
 using DigiShikshya.Infrastructure.Services;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 public static class InfrastructureServiceRegistration
 {
-    public static void AddInfrastructureServices(this IServiceCollection services)
+    public static void AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
         //Register database connection
         services.AddScoped<VideoService>();
         services.AddSignalR();
+        services.AddHttpClient<KeycloakService>(client =>
+        {
+                var keycloakUrl = configuration["KeyCloak:Url"];
+            client.BaseAddress = new Uri(keycloakUrl!);
+        });
 
 
 
