783640f4b6325f53afac73a9527d3f8626e8d9bd
techmologist <piyushrajbista@outlook.com>
2024-09-30 16:46:11 +0545

    feat: added auth controller

--- DigiShikshya-backend/DigiShikshya.API/Controller/Auth/AuthController.cs
+++ DigiShikshya-backend/DigiShikshya.API/Controller/Auth/AuthController.cs
@@ -1,28 +1,35 @@
-// using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 
-// public class AuthController:ControllerBase 
-// {
-//     private readonly KeycloakService _authService;
+public class AuthController:ControllerBase 
+{
+    private readonly KeycloakService _authService;
 
-//     public AuthController(KeycloakService authService)
-//     {
-//         _authService = authService;
+    public AuthController(KeycloakService authService)
+    {
+        _authService = authService;
        
-//     }
+    }
 
-//     [HttpPost("login")]
-//     public async Task<IActionResult> Login([FromBody] LoginRequest loginRequest)
-//     {
-//         var token = await _authService.AuthenticateAsync(loginRequest.Username, loginRequest.Password);
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginRequest loginRequest)
+    {
+        var token = await _authService.AuthenticateAsync(loginRequest.Username, loginRequest.Password);
 
-//         if (token == null)
-//         {
-//             return Unauthorized();
-//         }
+        if (token == null)
+        {
+            return Unauthorized();
+        }
 
-//         var userData = JwtTokenHelper.GetTokenInfo(token);
+        var userData = JwtTokenHelper.GetTokenInfo(token);
+        await _authService.RegisterAsync(userData.Id!);
 
-//         return Ok(new { Token = token, Info = userData });
-//     }
+        return Ok(new { Token = token, Info = userData });
+    }
     
-// }
+}
+
+public class LoginRequest
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
