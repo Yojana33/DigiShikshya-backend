4b9be79c9623fdf25d33bae4c8384aae6c4d9c09
Yojana <yosulalala@gmail.com>
2024-10-02 16:25:46 +0545

    service registration for student enrollment repository

--- DigiShikshya-backend/DigiShikshya.Persistence/ServiceRegistration.cs
+++ DigiShikshya-backend/DigiShikshya.Persistence/ServiceRegistration.cs
@@ -25,6 +25,7 @@ public static class ServiceRegistration
                 services.AddScoped<ITeacherAssignRepository, TeacherAssignRepository>();
 
                 services.AddScoped<IUserRepository, UserRepository>();
+                services.AddScoped<IStudentEnrollmentRepository, StudentEnrollmentRepository>();
         }
 }
 
