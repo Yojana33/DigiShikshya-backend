d714e919739678a91a24eef5c25f3c81a11531a5
techmologist <piyushrajbista@outlook.com>
2024-10-02 17:23:12 +0545

    refactor: Update KeycloakService to handle token refresh and logout

--- DigiShikshya-backend/DigiShikshya.Infrastructure/Auth/KeycloakService.cs
+++ DigiShikshya-backend/DigiShikshya.Infrastructure/Auth/KeycloakService.cs
@@ -4,14 +4,15 @@ using Microsoft.Extensions.Configuration;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Runtime.CompilerServices;
+using Microsoft.AspNetCore.Mvc;
 
-public class KeycloakService: IKeyClaokService
+public class KeycloakService : IKeyClaokService
 {
     private readonly HttpClient _httpClient;
     private readonly IConfiguration _configuration;
     private readonly IUserRepository _userRepository;
 
-    public KeycloakService(HttpClient httpClient, IConfiguration configuration,IUserRepository userRepository)
+    public KeycloakService(HttpClient httpClient, IConfiguration configuration, IUserRepository userRepository)
     {
         _httpClient = httpClient;
         _configuration = configuration;
@@ -32,7 +33,7 @@ public class KeycloakService: IKeyClaokService
 
         if (!response.IsSuccessStatusCode)
         {
-            return (null, null);
+            return (null, null)!;
         }
 
         var responseContent = await response.Content.ReadAsStringAsync();
@@ -40,7 +41,7 @@ public class KeycloakService: IKeyClaokService
         try
         {
             var tokenResponse = JsonSerializer.Deserialize<JsonElement>(responseContent);
-            return (tokenResponse.GetProperty("access_token").GetString(), tokenResponse.GetProperty("refresh_token").GetString());
+            return (tokenResponse.GetProperty("access_token").GetString(), tokenResponse.GetProperty("refresh_token").GetString())!;
         }
         catch (JsonException)
         {
@@ -48,10 +49,54 @@ public class KeycloakService: IKeyClaokService
         }
     }
 
+        public async Task<bool> LogoutAsync(string refreshToken)
+    {
+        var keycloakSettings = _configuration.GetSection("KeyCloak");
+    
+        var requestBody = new StringContent(
+            $"client_id={keycloakSettings["ClientId"]}&client_secret={keycloakSettings["ClientSecret"]}&refresh_token={refreshToken}",
+            Encoding.UTF8,
+            "application/x-www-form-urlencoded");
+    
+        var response = await _httpClient.PostAsync($"/realms/{keycloakSettings["Realm"]}/protocol/openid-connect/logout", requestBody);
+    
+        return response.IsSuccessStatusCode;
+    }
+
+    public async Task<(string,string)> GetNewAccessByRefreshToken (string refreshToken)
+    {
+        var keycloakSettings = _configuration.GetSection("KeyCloak");
+    
+        var requestBody = new StringContent(
+            $"client_id={keycloakSettings["ClientId"]}&client_secret={keycloakSettings["ClientSecret"]}&grant_type=refresh_token&refresh_token={refreshToken}",
+            Encoding.UTF8,
+            "application/x-www-form-urlencoded");
+    
+        var response = await _httpClient.PostAsync($"/realms/{keycloakSettings["Realm"]}/protocol/openid-connect/token", requestBody);
+    
+        if (!response.IsSuccessStatusCode)
+        {
+            return (null, null)!;
+        }
+    
+        var responseContent = await response.Content.ReadAsStringAsync();
+    
+        try
+        {
+            var tokenResponse = JsonSerializer.Deserialize<JsonElement>(responseContent);
+            return (tokenResponse.GetProperty("access_token").GetString(), tokenResponse.GetProperty("refresh_token").GetString())!;
+        }
+        catch (JsonException)
+        {
+            throw;
+        }
+
+    }
+
     public async Task<bool> RegisterAsync(string id)
     {
         return await _userRepository.AddUserWhenLoggedIn(id);
     }
 
-    
+
 }
